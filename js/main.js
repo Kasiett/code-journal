320bb321d5a2288217dba0913cd92944d9c297f7
Kasiet Rakhimdinova <k.rakhimdinova@gmail.com>
2023-04-11 21:54:02 +0000

    Added $ to variables which are  referencing elements from the DOM

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-const input = document.querySelector('#url');
-const img = document.querySelector('img');
-const attributeValue = img.getAttribute('src');
+const $input = document.querySelector('#url');
+const $img = document.querySelector('img');
+const $attributeValue = $img.getAttribute('src');
 
-input.addEventListener('input', function (e) {
-  img.setAttribute('src', e.target.value);
+$input.addEventListener('input', function (e) {
+  $img.setAttribute('src', e.target.value);
 });
 
-const form = document.querySelector('form');
-form.addEventListener('submit', function (e) {
+const $form = document.querySelector('form');
+$form.addEventListener('submit', function (e) {
   e.preventDefault();
   const formData = {
-    title: form.elements.title.value,
-    url: form.elements.url.value,
-    textarea: form.elements.textarea.value
+    title: $form.elements.title.value,
+    url: $form.elements.url.value,
+    textarea: $form.elements.textarea.value
   };
 
   formData.entryId = data.nextEntryId++;
   data.entries.unshift(formData);
-  img.setAttribute('src', attributeValue);
-  form.reset();
+  $img.setAttribute('src', $attributeValue);
+  $form.reset();
 });
