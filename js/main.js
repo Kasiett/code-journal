9493e4704d4b41ac17bacee45c4300557c099735
Kasiet Rakhimdinova <k.rakhimdinova@gmail.com>
2023-04-11 17:21:32 +0000

    Created new object added new obj to entries array, reseted preview img to placeholder img reset the form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const input = document.querySelector('#url');
 const img = document.querySelector('img');
+const attributeValue = img.getAttribute('src');
 input.addEventListener('input', function (e) {
   img.setAttribute('src', e.target.value);
 });
@@ -10,11 +11,13 @@ const form = document.querySelector('form');
 // const textArea = document.querySelector('#textarea');
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-  // const formData = {
-  //   title: form.elements.title.value,
-  //   url: form.elements.url.value,
-  //   textarea: form.elements.textarea.value
-  // };
-
+  const formData = {
+    title: form.elements.title.value,
+    url: form.elements.url.value,
+    textarea: form.elements.textarea.value
+  };
+  formData.entryId = data.nextEntryId++;
+  data.entries.unshift(formData);
+  img.setAttribute('src', attributeValue);
   form.reset();
 });
