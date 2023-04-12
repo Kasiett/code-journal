0566a5b175d5d4b2873b0b1525016eee6a1cca2d
Kasiet Rakhimdinova <k.rakhimdinova@gmail.com>
2023-04-12 04:44:06 +0000

    Created Dom tree for entrys page, added white-space: pre-line to div.column-half >  p tag

--- js/main.js
+++ js/main.js
@@ -20,3 +20,45 @@ $form.addEventListener('submit', function (e) {
   $img.setAttribute('src', $attributeValue);
   $form.reset();
 });
+
+/// ////Code for entry page below
+
+function renderEntry(entry) {
+  const divDataView = document.createElement('div');
+  divDataView.setAttribute('data-view', 'entries');
+
+  const ul = document.createElement('ul');
+  divDataView.appendChild(ul);
+
+  const list = document.createElement('li');
+  ul.appendChild(list);
+
+  const firstDivColHalf = document.createElement('div');
+  firstDivColHalf.setAttribute('class', 'column-half');
+  list.appendChild(firstDivColHalf);
+
+  const entriesImg = document.createElement('img');
+  entriesImg.setAttribute('class', 'entries-img');
+  entriesImg.setAttribute('src', entry.url);
+  entriesImg.setAttribute('alt', 'user image');
+  firstDivColHalf.appendChild(entriesImg);
+
+  const secondDivColHalf = document.createElement('div');
+  secondDivColHalf.setAttribute('class', 'column-half');
+  list.appendChild(secondDivColHalf);
+
+  const h2Title = document.createElement('h2');
+  h2Title.setAttribute('class', 'entries-h2');
+  const title = document.createTextNode(entry.title);
+  h2Title.appendChild(title);
+  secondDivColHalf.appendChild(h2Title);
+
+  const paragraph = document.createElement('p');
+  paragraph.setAttribute('class', 'entries-p');
+  const paraTextOne = document.createTextNode(entry.textarea);
+  paragraph.appendChild(paraTextOne);
+  secondDivColHalf.appendChild(paragraph);
+
+  return divDataView;
+}
+renderEntry(data.entries[0]);
